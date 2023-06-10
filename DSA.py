d9ba9d8eb045066b7755a974db925535483be17a
IzzyBoi69 <soumyabanerjee2002@gmail.com>
2023-06-10 20:59:22 +0530

    Initial commit

---
+++ DSA.py
@@ -0,0 +1,34 @@
+states_of_india = ["West Bengal", "Maharashtra", "Delhi", "Tamil Nadu", "Karnataka" ]
+# print(states_of_india[1])
+# print(states_of_india[-1])
+
+# states_of_india[0]= "Kolkata"
+# states_of_india[1]= "Mumbai"
+
+# states_of_india.extend(["Soumyaland" , "Andaman and Nicobar Island"])
+# print(states_of_india)
+
+# states_of_india.append("Chennai")
+# print(states_of_india)
+
+# states_of_india.insert(4, "Bangalore")
+# print(states_of_india)
+
+# states_of_india.remove("West Bengal")
+# print(states_of_india)
+
+# states_of_india.pop(2)
+# print(states_of_india)
+
+# states_of_india.clear()
+# print(states_of_india)
+
+# states_of_india.index('Delhi')
+# print(states_of_india)
+
+# states_of_india.reverse()
+# print(states_of_india)
+
+# states_of_india.sort()
+# print(states_of_india)
+
---
+++ string to list.py
@@ -0,0 +1,8 @@
+string1 = "Python is great"
+
+print("Actual string: ", string1)
+print("The type of string: ", type(string1))
+print("String converted to list:", string1.split())
+print("String converted to list: \n", list(string1))
+
+
